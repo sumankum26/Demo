b5cf8bc3948cc257401ae108ccd7bfe114290fea
Sinsha <Sinsha@DESKTOP-N55D2GM>
2021-07-02 22:16:45 +0530

    Add project files.

---
+++ Demo/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private static readonly List<Demo.Model.User> Data = new List<Model.User>
+        {            
+                new Model.User{ UserId = "1", FirstName ="Tom", LastName="Cruise",Role="Actor",Location="New York",IsActive=true}
+        };
+
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IList<Demo.Model.User> Get()
+        {
+            return Data;
+        }
+
+
+    }
+}
---
+++ Demo/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Model
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+        public string Location { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
